6d9f729ef15827c14b434e0138664f463c02ee26
Grzegorz Paprzycki <grzegorz.paprzycki@capgemini.com>
2023-04-05 19:15:28 +0200

    create API functions for register and signin

---
+++ lib/api.ts
@@ -0,0 +1,34 @@
+type IParams = {
+  url: string;
+  method: "POST" | "GET" | "PUT" | "DELETE" | "PATCH";
+  body?: any;
+  json?: boolean;
+};
+
+const fetcher = async ({ url, method, body, json = true }: IParams) => {
+  const res = await fetch(url, {
+    method,
+    ...(body && { body: JSON.stringify(body) }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Error on API side");
+  }
+
+  if (json) {
+    const data = await res.json();
+    return data.data;
+  }
+};
+
+export const register = (user) => {
+  return fetcher({ url: "/api/register", method: "POST", body: user });
+};
+
+export const signin = (user) => {
+  return fetcher({ url: "/api/signin", method: "POST", body: user });
+};
