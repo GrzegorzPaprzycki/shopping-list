00788ef9877ffbf3cf5aca0e087b78cacc5127cc
Grzegorz Paprzycki <grzegorz.paprzycki@capgemini.com>
2023-04-14 14:05:05 +0200

    create GreetingSkeleton for streaming

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,11 +1,15 @@
 import Greetings from "@/components/Greetings";
+import GreetingsSkeleton from "@/components/GreetingsSkeleton";
+import { Suspense } from "react";
 
 export default async function Page() {
   return (
-    <div className="h-full overflow-y-auto pr-6 w-1/1">
+    <div className="h-full overflow-y-auto pr-6 w-full">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">
-          <Greetings />
+          <Suspense fallback={<GreetingsSkeleton />}>
+            <Greetings />
+          </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {/** shopping lists map here */}
